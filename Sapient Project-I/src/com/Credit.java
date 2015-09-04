4649e8d4318da9b7346ea5a02484efe8d182bd4a
unknown <vgar11@deltahiti46613.sapient.com>
2015-09-04 10:32:26 +0530

    Order and Credit Class Completed

--- Sapient Project-I/src/com/Credit.java
+++ Sapient Project-I/src/com/Credit.java
@@ -1,5 +1,23 @@
 package com;
 
 public class Credit extends Payment implements Authorization{
-
+	private int number;
+	public int getNumber() {return number;}
+	public void setNumber(int number) {this.number = number;}
+	
+	
+	private String type;
+	public String getType() {return type;}
+	public void setType(String type) {this.type = type;}
+	
+	private String expDate;
+	public String getExpDate() {return expDate;}
+	public void setExpDate(String expDate) {this.expDate = expDate;}
+	public boolean authorization() {return true;}
+	
+	@Override
+	public boolean authorized() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- Sapient Project-I/src/com/Order.java
+++ Sapient Project-I/src/com/Order.java
@@ -1,5 +1,35 @@
 package com;
 
 public class Order {
+	private String date;
+	public String getDate() {return date;}
+	public void setDate(String date) {this.date = date;}
+	
+	
+	private String status;
+	public String getStatus() {return status;}
+	public void setStatus(String status) {this.status = status;}
+	
+	
+	private NewCustomer customer;
+	public NewCustomer getCustomer() {return customer;}
+	public void setCustomer(NewCustomer customer) {this.customer = customer;}
+	
+	private int payment;
+	public int getPayment() {return payment;}
+	public void setPayment(int payment) {this.payment = payment;}
+	
+	
+	private OrderDetail orderdetail = new OrderDetail();
+	public OrderDetail getOrderdetail() {return orderdetail;}
+	public void setOrderdetail(OrderDetail orderdetail) {this.orderdetail = orderdetail;}
+	
+	
+	public void calcTax(){
+	}
+	public void calcTotal(){
+	}
+	public void calctotalWeight(){
+	}
 
 }
